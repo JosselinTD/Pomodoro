533c91ff0c98d0e5be8738ef6b1782ec59173eb7
Josselin <josselin@bemyapp.com>
2015-12-28 12:36:41 +0100

    Yeoman angular fullstack  initialisation

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('pomodoroApp.admin', [
+  'pomodoroApp.auth',
+  'ngRoute'
+]);
---
+++ client/components/oauth-buttons/oauth-buttons.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('pomodoroApp')
+  .controller('OauthButtonsCtrl', function($window) {
+    this.loginOauth = function(provider) {
+      $window.location.href = '/auth/' + provider;
+    };
+  });
